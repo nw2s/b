5866e6e7096a7e7006944e5215f3561d7cd7ba92
nw2s <thomas.scott.wilson@gmail.com>
2014-02-17 22:26:05 -0500

    Updating test sequence a bit.

--- sketches/nw2s/bTestSequence1/bTestSequence1.ino
+++ sketches/nw2s/bTestSequence1/bTestSequence1.ino
@@ -38,7 +38,8 @@ void setup()
 
 	EventManager::initialize();
 
-	FixedClock* fixedclock = FixedClock::create(120, 16);
+	Clock* vclock = VariableClock::create(30, 30, DUE_IN_A00, 16);
+	//FixedClock* fixedclock = FixedClock::create(120, 16);
 
 	/* Iterate over octaves on all the outputs for tuning */
 	SequenceNote notelist3[6] = { {0,1}, {1,1}, {2,1}, {3,1}, {4,1}, {5,1} };
@@ -61,72 +62,60 @@ void setup()
 	NoteSequencer* s15 = NoteSequencer::create(notes3, C, MAJOR, DIV_QUARTER, DUE_SPI_4822_14, false);
 	NoteSequencer* s16 = NoteSequencer::create(notes3, C, MAJOR, DIV_QUARTER, DUE_SPI_4822_15, false);
 	
-	fixedclock->registerdevice(s01);
-	fixedclock->registerdevice(s02);
-	fixedclock->registerdevice(s03);
-	fixedclock->registerdevice(s04);
-	fixedclock->registerdevice(s05);
-	fixedclock->registerdevice(s06);
-	fixedclock->registerdevice(s07);
-	fixedclock->registerdevice(s08);
-	fixedclock->registerdevice(s09);
-	fixedclock->registerdevice(s10);
-	fixedclock->registerdevice(s11);
-	fixedclock->registerdevice(s12);
-	fixedclock->registerdevice(s13);
-	fixedclock->registerdevice(s14);
-	fixedclock->registerdevice(s15);
-	fixedclock->registerdevice(s16);
+	vclock->registerDevice(s01);
+	vclock->registerDevice(s02);
+	vclock->registerDevice(s03);
+	vclock->registerDevice(s04);
+	vclock->registerDevice(s05);
+	vclock->registerDevice(s06);
+	vclock->registerDevice(s07);
+	vclock->registerDevice(s08);
+	vclock->registerDevice(s09);
+	vclock->registerDevice(s10);
+	vclock->registerDevice(s11);
+	vclock->registerDevice(s12);
+	vclock->registerDevice(s13);
+	vclock->registerDevice(s14);
+	vclock->registerDevice(s15);
+	vclock->registerDevice(s16);
 
-	EventManager::registerdevice(fixedclock);
+	EventManager::registerDevice(vclock);
 	
 
 	/* Setup triggers on all the outputs to make sure they're working */
 	Trigger* trigger01 = Trigger::create(DUE_OUT_D00, DIV_WHOLE);
-	Trigger* trigger02 = Trigger::create(DUE_OUT_D08, DIV_HALF_DOT);
-	Trigger* trigger03 = Trigger::create(DUE_OUT_D01, DIV_HALF);
-	Trigger* trigger04 = Trigger::create(DUE_OUT_D09, DIV_QUARTER_DOT);
-	Trigger* trigger05 = Trigger::create(DUE_OUT_D02, DIV_QUARTER);
-	Trigger* trigger06 = Trigger::create(DUE_OUT_D10, DIV_QUARTER_TRIPLET);
-	Trigger* trigger07 = Trigger::create(DUE_OUT_D03, DIV_EIGHTH_DOT);
-	Trigger* trigger08 = Trigger::create(DUE_OUT_D11, DIV_EIGHTH);
-	Trigger* trigger09 = Trigger::create(DUE_OUT_D04, DIV_EIGHTH_TRIPLET);
-	Trigger* trigger10 = Trigger::create(DUE_OUT_D12, DIV_SIXTEENTH_DOT);
-	Trigger* trigger11 = Trigger::create(DUE_OUT_D05, DIV_SIXTEENTH);
-	Trigger* trigger12 = Trigger::create(DUE_OUT_D13, DIV_SIXTEENTH_TRIPLET);
-	Trigger* trigger13 = Trigger::create(DUE_OUT_D06, DIV_THIRTYSECOND_DOT);
-	Trigger* trigger14 = Trigger::create(DUE_OUT_D14, DIV_THIRTYSECOND);
-	Trigger* trigger15 = Trigger::create(DUE_OUT_D07, DIV_THIRTYSECOND_TRIPLET);
+	Trigger* trigger02 = Trigger::create(DUE_OUT_D01, DIV_HALF_DOT);
+	Trigger* trigger03 = Trigger::create(DUE_OUT_D02, DIV_HALF);
+	Trigger* trigger04 = Trigger::create(DUE_OUT_D03, DIV_QUARTER_DOT);
+	Trigger* trigger05 = Trigger::create(DUE_OUT_D04, DIV_QUARTER);
+	Trigger* trigger06 = Trigger::create(DUE_OUT_D05, DIV_QUARTER_TRIPLET);
+	Trigger* trigger07 = Trigger::create(DUE_OUT_D06, DIV_EIGHTH_DOT);
+	Trigger* trigger08 = Trigger::create(DUE_OUT_D07, DIV_EIGHTH);
+	Trigger* trigger09 = Trigger::create(DUE_OUT_D08, DIV_EIGHTH_TRIPLET);
+	Trigger* trigger10 = Trigger::create(DUE_OUT_D09, DIV_SIXTEENTH_DOT);
+	Trigger* trigger11 = Trigger::create(DUE_OUT_D10, DIV_SIXTEENTH);
+	Trigger* trigger12 = Trigger::create(DUE_OUT_D11, DIV_SIXTEENTH_TRIPLET);
+	Trigger* trigger13 = Trigger::create(DUE_OUT_D12, DIV_THIRTYSECOND_DOT);
+	Trigger* trigger14 = Trigger::create(DUE_OUT_D13, DIV_THIRTYSECOND);
+	Trigger* trigger15 = Trigger::create(DUE_OUT_D14, DIV_THIRTYSECOND_TRIPLET);
 	Trigger* trigger16 = Trigger::create(DUE_OUT_D15, DIV_QUARTER);
 
-	fixedclock->registerdevice(trigger01);
-	fixedclock->registerdevice(trigger02);
-	fixedclock->registerdevice(trigger03);
-	fixedclock->registerdevice(trigger04);
-	fixedclock->registerdevice(trigger05);
-	fixedclock->registerdevice(trigger06);
-	fixedclock->registerdevice(trigger07);
-	fixedclock->registerdevice(trigger08);
-	fixedclock->registerdevice(trigger09);
-	fixedclock->registerdevice(trigger10);
-	fixedclock->registerdevice(trigger11);
-	fixedclock->registerdevice(trigger12);
-	fixedclock->registerdevice(trigger13);
-	fixedclock->registerdevice(trigger14);
-	fixedclock->registerdevice(trigger15);
-	fixedclock->registerdevice(trigger16);
-	
-	
-	//TODO: Setup a simple square wave on two analog outputs for level setting
-	
-	
-	//TODO: Watch analog input changes and write the values that change
-	
-	
-	//TODO: Watch digital input changes and write the values that change
-	
-	
-	
+	vclock->registerDevice(trigger01);
+	vclock->registerDevice(trigger02);
+	vclock->registerDevice(trigger03);
+	vclock->registerDevice(trigger04);
+	vclock->registerDevice(trigger05);
+	vclock->registerDevice(trigger06);
+	vclock->registerDevice(trigger07);
+	vclock->registerDevice(trigger08);
+	vclock->registerDevice(trigger09);
+	vclock->registerDevice(trigger10);
+	vclock->registerDevice(trigger11);
+	vclock->registerDevice(trigger12);
+	vclock->registerDevice(trigger13);
+	vclock->registerDevice(trigger14);
+	vclock->registerDevice(trigger15);
+	vclock->registerDevice(trigger16);	
 }
 
 void loop() 
